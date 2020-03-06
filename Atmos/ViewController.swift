3645b438952eae6075ae1b8d9e0ebac173795aa1
Dakota Kim <codingvampire@gmail.com>
2020-03-06 13:07:32 -0500

    Initial Commit

---
+++ Atmos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Atmos
+//
+//  Created by Dakota Cantina on 3/6/20.
+//  Copyright © 2020 Dakota Cantina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
